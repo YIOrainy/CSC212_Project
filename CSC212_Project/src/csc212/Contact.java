29711aef06519076fafeaeafa8a82d81d6950519
Osamah Sarraj <osamah.sarraj@gmail.com>
2023-10-06 22:41:25 +0300

    Update Contact.java

--- CSC212_Project/src/csc212/Contact.java
+++ CSC212_Project/src/csc212/Contact.java
@@ -1,91 +1,63 @@
 package csc212;
 
+import java.util.Objects;
+
 public class Contact implements Comparable<Contact> {
-	
-//	Constructor
-	public Contact(String name, String phoneNumber, String emailAddress, String address, String birthday, String note) {
-		this.name = name;
-		this.phoneNumber = phoneNumber;
-		this.emailAddress = emailAddress;
-		this.address = address;
-		this.birthday = birthday;
-		this.note = note;
-	}
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+    private String address;
+    private String birthday;
+    private String notes;
 
-//	Attributes
-	private String name;
-	private String phoneNumber;
-	private String emailAddress;
-	private String address;
-	private String birthday;
-	private String note;
+    public Contact(String name, String phoneNumber, String emailAddress, String address, String birthday, String notes) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.address = address;
+        this.birthday = birthday;
+        this.notes = notes;
+    }
 
-//	Attributes
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
+    public String getEmailAddress() {
+        return emailAddress;
+    }
 
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+    public String getAddress() {
+        return address;
+    }
 
-	public String getEmailAddress() {
-		return emailAddress;
-	}
+    public String getBirthday() {
+        return birthday;
+    }
 
-	public void setEmailAddress(String emailAddress) {
-		this.emailAddress = emailAddress;
-	}
+    public String getNotes() {
+        return notes;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    @Override
+    public int compareTo(Contact other) {
+        return this.name.compareTo(other.name);
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Contact contact = (Contact) obj;
+        return name.equals(contact.name) || phoneNumber.equals(contact.phoneNumber);
+    }
 
-	public String getBirthday() {
-		return birthday;
-	}
-
-	public void setBirthday(String birthday) {
-		this.birthday = birthday;
-	}
-
-	public String getNote() {
-		return note;
-	}
-
-	public void setNote(String note) {
-		this.note = note;
-	}
-
-// == Note : i am not sure if the method print in the wright place or no ==
-	public void print() {
-		System.out.println("Name: "+getName());
-		System.out.println("PhoneNumber: "+getPhoneNumber());
-		System.out.println("Email Address: "+getEmailAddress());
-		System.out.println("Adderss: "+getAddress());
-		System.out.println("Birthday: "+getBirthday());
-		System.out.println("Notes: "+getNote());
-		System.out.println(" ");
-
-
-	}
-
-	public int compareTo(Contact name) {
-		return this.name.compareTo(name.getName());
-	}
-
-	
-	
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phoneNumber);
+    }
 }

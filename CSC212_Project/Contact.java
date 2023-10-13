b3850bb41afa6bf56f721700e6072b9aeab3a829
Osamah Sarraj <osamah.sarraj@gmail.com>
2023-10-13 14:00:06 +0300

    Fixing the Contact and make each one has a list of Events

--- CSC212_Project/Contact.java
+++ CSC212_Project/Contact.java
@@ -1,6 +1,6 @@
 package CSC212_Project; // This specifies the package to which this class belongs.
 
-import java.util.Objects; // This imports the Objects class which provides utility methods for working with objects, such as null-safe comparisions and hash code calculations.
+import java.util.Date;
 
 // The Contact class implements the Comparable interface, meaning it can be compared to other Contacts.
 public class Contact implements Comparable<Contact> {
@@ -10,63 +10,76 @@ public class Contact implements Comparable<Contact> {
     private String phoneNumber;
     private String emailAddress;
     private String address;
-    private String birthday;
+    private Date birthday;
     private String notes;
+    private LinkedListADT<Event> events;
 
-    // This is the constructor for the Contact class.
-    public Contact(String name, String phoneNumber, String emailAddress, String address, String birthday, String notes) {
+    // Constructor to initialize the contact with an empty state.
+    public Contact() {
+        this.name = "";
+        this.phoneNumber = "";
+        this.emailAddress = "";
+        this.address = "";
+        this.birthday = null;
+        this.notes = "";
+        events = new LinkedListADT<Event>();
+    }
+
+    // Constructor to initialize the contact with the provided values.
+    public Contact(String name, String phoneNumber, String emailAddress, String address, String birthday,
+            String notes) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.emailAddress = emailAddress;
         this.address = address;
-        this.birthday = birthday;
+        this.birthday = new Date(birthday);
         this.notes = notes;
+        events = new LinkedListADT<Event>();
     }
 
-    // These are the getter methods that provide access to the contact properties.
-    public String getName() {
-        return name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getBirthday() {
-        return birthday;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    // This method is called when a Contact object is printed.
-    // This method is used to compare two contacts based on their names.
     @Override
-    public int compareTo(Contact other) {
-        return this.name.compareTo(other.name);
+    public String toString() {
+        return "\nName: " + name +
+                "\nPhone Number: " + phoneNumber +
+                "\nEmail Address: " + emailAddress +
+                "\nAddress: " + address +
+                "\nBirthday: " + birthday +
+                "\nNotes: " + notes + "\n";
     }
 
-    // This method checks if two contacts are equal based on their name or phone number.
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Contact contact = (Contact) obj;
-        return name.equals(contact.name) || phoneNumber.equals(contact.phoneNumber);
+    public boolean addEvent(Event e) {
+        if (!events.empty()) {
+            events.findFirst();
+            for (int i = 0; i < events.size; i++) {
+                if ((events.retrieve().date.compareTo(e.date) == 0)
+                        && (events.retrieve().time.compareTo(e.time) == 0))
+                    return false;
+            }
+        }
+        events.insertSort(e);
+        return true;
     }
 
-    // This method returns a hash code for the contact based on its name and phone number.
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, phoneNumber);
+    public boolean removeEvent(String eTitle) {
+        if (events.empty())
+            return false;
+        Event val = new Event();
+        val.title = eTitle;
+        if (events.search(val)) {
+            events.remove(val);
+            return true;
+        }
+        return false;
     }
+
+    @Override
+    public int compareTo(Contact o) {
+        try {
+            return (this.name.compareTo(o.name));
+        } catch (Exception e) {
+            // To change body of generated methods, choose Tools | Templates.
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+
 }

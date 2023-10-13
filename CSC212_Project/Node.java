5c263fb121650a0aee7ddd30a6dcf21b2dbe1880
Osamah Sarraj <osamah.sarraj@gmail.com>
2023-10-13 13:37:28 +0300

    Adding Comments to explain

--- CSC212_Project/Node.java
+++ CSC212_Project/Node.java
@@ -1,12 +1,13 @@
 package CSC212_Project;
 
-public class Node<T>{
+// Node class represents a node in a linked list.
+public class Node<T> {
 
     public T data; // Data stored in the node.
     public Node<T> next; // Reference to the next node in the linked list.
 
     // Default constructor: Initializes data and next to null.
-    public Node () {
+    public Node() {
         data = null;
         next = null;
     }
@@ -14,25 +15,28 @@ public class Node<T>{
     // Constructor with data parameter: Initializes data with the provided value,
     // and sets next to null (indicating that this node is initially not connected
     // to any other node).
-    public Node (T val) {
+    public Node(T val) {
         data = val;
         next = null;
     }
 
     // Setters/Getters...
-
+    // Getter for retrieving the data stored in the node.
     public T getData() {
         return data;
     }
 
+    // Setter for updating the data stored in the node.
     public void setData(T data) {
         this.data = data;
     }
 
+    // Getter for retrieving the reference to the next node.
     public Node<T> getNext() {
         return next;
     }
-
+    
+    // Setter for updating the reference to the next node.
     public void setNext(Node<T> next) {
         this.next = next;
     }
